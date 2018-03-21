82d70371679de636786cca71e2feb2a905886a65
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-21 20:59:51 +0000

    automatically backed up by learn

--- spec/features/05_song_form_spec.rb
+++ spec/features/05_song_form_spec.rb
@@ -114,7 +114,7 @@ describe "Song Forms" do
       it "has a checkbox element on the form" do
         expect(page.body).to include("checkbox")
       end
-      binding.pru
+      binding.pry
 
       it "updates the song's genres" do
         uncheck "New Age Garbage"
